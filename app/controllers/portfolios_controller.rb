540bcbfe38f169c92940f7478af7ea3fafafbc42
Jared Murray <jared@agileblocks.io>
2018-10-22 15:22:30 -0400

    Added edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,50 @@
+# frozen_string_literal: true
+
 class PortfoliosController < ApplicationController
-    def index
-        @portfolio_items = Portfolio.all
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Blog.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item, notice: 'Your portflio is now live' }
+      else
+        format.html { render :new }
+      end
     end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to @portfolio_item, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def portfolio_item_params
+    params.require(:portfolio_item).permit(:title, :body, :subtitle)
+  end
 end
