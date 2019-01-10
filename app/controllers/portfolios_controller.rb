6600129cf28b751acac143e1624c7cad403c9199
Jared Murray <jared.murray@shopify.com>
2019-01-10 13:17:50 -0500

    Implemented form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,6 +16,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -59,6 +60,6 @@ class PortfoliosController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def portfolio_item_params
-    params.require(:portfolio).permit(:title, :body, :subtitle)
+    params.require(:portfolio).permit(:title, :body, :subtitle, technologies_attributes: [:name])
   end
 end
