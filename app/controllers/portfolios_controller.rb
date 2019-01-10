0e945a191783bacbc22430606eb584bd37db7e61
Jared Murray <jared.murray@shopify.com>
2019-01-10 16:30:01 -0500

    Reformatted params for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -60,6 +60,10 @@ class PortfoliosController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def portfolio_item_params
-    params.require(:portfolio).permit(:title, :body, :subtitle, technologies_attributes: [:name])
+    params.require(:portfolio).permit(:title, 
+                                      :body, 
+                                      :subtitle, 
+                                      technologies_attributes: [:name]
+                                      )
   end
 end
